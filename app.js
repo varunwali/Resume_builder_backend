09936e1330b4590730eeeacd96dcad0d1a055a07
varunwali <varunwali36@gmail.com>
2024-03-06 23:37:10 +0530

    commit

--- app.js
+++ app.js
@@ -9,26 +9,24 @@ import { errorMiddleware } from "./middlewares/error.js";
 const app = express(); //creating the express instance
 config({ path: "./config/config.env" }); //loading environment variables
 
-// Define allowed origins
-const allowedOrigins = ["https://resume-builder-frontend-amber.vercel.app/"];
+const allowedOrigins = ["https://resume-builder-frontend-amber.vercel.app"];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Check if the origin is in the allowed origins list
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"], //these are the allowed request types to be made by the user
-    credentials: true, //this allows us to use cookies in our application
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
-); //enabling CORS to allow requests from different origins
+);
 
 app.use(cookieParser());
-app.use(express.json()); //parsing incoming requests with JSON payloads
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/user", userRoute);
